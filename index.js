fe89931131ba12d32f439fef5d09f494e8df90af
ana-lapas <anapaulalapas@gmail.com>
2022-11-14 11:56:13 -0400

    Posts ajustados e get participants

--- index.js
+++ index.js
@@ -12,95 +12,104 @@ const userSchema = joi.object({
 const messageSchema = joi.object({
     to: joi.string().required().min(3),
     text: joi.string().required().min(3),
-    type: "private_message"
+    type: joi.string().valid("message", "private_message").required()
 })
 //Configs
 app.use(cors())
 app.use(express.json())
 dotenv.config()
-
 const mongoClient = new MongoClient(process.env.MONGO_URI)
+
+try {
+    await mongoClient.connect()
+} catch (err) {
+    console.log(err.message)
+}
 let db;
 db = mongoClient.db('databankp13')
 const collectionParticipantsOn = db.collection("participantsOn")
 const collectionMessagesHistory = db.collection("messagesSaved")
-
-try {
-    await mongoClient.connect()
-    console.log("MongoDB is connected")
-} catch (err) {
-    console.log("Erro ao conectar com o MongoDB")
-    console.log(err)
-}
-
 app.post("/participants", async (req, res) => {
-    console.log("entrou no post participants")
-    const newUser = req.body
-    console.log(newUser)
-    const validation = userSchema.validate(newUser)
-    console.log(validation, { abortEarly: false })
+    const userName = req.body
+    const validation = userSchema.validate(userName, { abortEarly: false })
     if (validation.error) {
-        console.log("entrou no erro")
-        console.log(validation.error.details)
         const erros = validation.error.details.map((detail) => detail.message);
         res.status(422).send(erros);
     }
 
     try {
-        const checkUser = await collectionParticipantsOn.findOne({ name: newUser })
+        const checkUser = await collectionParticipantsOn.findOne({name: userName.name})
         if (!checkUser) {
-            let hora = dayjs().format('HH:MM:SS')
-            console.log(hora)
-            await collectionParticipantsOn.insertOne({ name: newUser, lastStatus: Date.now() })
-            await collectionMessagesHistory.insertOne({ from: newUser, to: 'Todos', text: 'entra na sala...', type: 'status', time: hora })
+            let hora = dayjs().format('HH:mm:ss')
+            await collectionParticipantsOn.insertOne({name: userName.name, lastStatus: Date.now()})
+            await collectionMessagesHistory.insertOne({ from: userName.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: hora })
             res.sendStatus(201)
         }
-    } catch (err) {
         res.sendStatus(409)
+    } catch (err) {
+        res.sendStatus(500)
     }
 })
 
 app.get("/participants", async (req, res) => {
-    console.log("Entrou no get participants")
     try {
         const usersOnline = await collectionParticipantsOn.find().toArray()
         res.send(usersOnline)
     } catch (err) {
-        console.log(err)
         res.sendStatus(500)
     }
 
 })
+
 app.post("/messages", async (req, res) => {
-    console.log("Entrou no post messages")
-    collectionMessagesHistory
-})
-app.get("/messages", async (req, res) => {
-    console.log("Entrou no get messages")
+    const newMessage = req.body
+    const validationM = messageSchema.validate(newMessage, { abortEarly: false })
+    if (validationM.error) {
+        const erros = validationM.error.details.map((detail) => detail.message);
+        res.status(422).send(erros);
+    }    
+    
     try {
-        const messagesEx = await collectionParticipantsOn.find().toArray()
-        res.send(usersOnline)
+        const checkFrom = req.headers.user
+        const checkUser = await collectionParticipantsOn.findOne({checkFrom})
+        if (!checkUser) {
+            res.sendStatus(404)
+        } 
+        let hora = dayjs().format('HH:mm:ss')
+        await collectionMessagesHistory.insertOne({ from: checkFrom, to: newMessage.to, text: newMessage.text, type: newMessage.message, time: hora })
+        res.sendStatus(201)
     } catch (err) {
-        console.log(err)
+        res.sendStatus(500)
+    }    
+})
+
+app.get("/messages", async (req, res) => {
+    const limit = parseInt(req.query.limit);
+    try {
+        const messagesEx = await collectionMessagesHistory.find().toArray()
+        res.send(messagesEx)
+    } catch (err) {
         res.sendStatus(500)
     }
 })
-app.post("/status/:user", async (req, res) => {
-    const { user } = req.params
+
+app.post("/status", async (req, res) => {
+    const userToBeUpdated = req.headers.user
     try {
-        await collectionParticipantsOn.findOne({ name: user })
-        if (!user) {
+        await collectionParticipantsOn.findOne({ name: userToBeUpdated }).toArray()
+        if (!userToBeUpdated) {
             res.sendStatus(404)
             return;
         }
-        await collectionParticipantsOn.updateOne({ 
-			name: user
-		}, { $currentDate: {lastStatus: true,  "lastStatus": { $type: "timestamp" } }})
-				
-		res.sendStatus(200)
+        
+        await collectionParticipantsOn.updateOne({
+            name: userToBeUpdated
+        }, { $set: {lastStatus: Date.now()} })
+        res.sendStatus(200)
     } catch {
-        console.log(err)
         res.sendStatus(404)
     }
 })
+
+/*setInterval( ,1500)*/
 app.listen(5000, () => console.log('Server running in port: 5000'))
